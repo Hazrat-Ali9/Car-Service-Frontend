6b16916b7b3834f6ad7356dec11eb6f14a1d136a
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-05-16 22:24:43 +0600

    service 🚠

--- src/components/Home/Services.jsx
+++ src/components/Home/Services.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-// Service
+// service
 import { useEffect, useState } from "react";
 import api from "../../util/Api";
 import { Link } from "react-router-dom";
